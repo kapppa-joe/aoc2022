62d6f6b8c21bd571183f065093c15fe35fca550c
kapppa.joe@gmail.com <joe.fong@madetech.com>
2022-12-05 09:51:24 +0000

    add day 05

---
+++ solutions/day_05.py
@@ -0,0 +1,73 @@
+import re
+import aoc_helper
+
+
+def parse_raw(raw: str) -> dict:
+    crates_raw, instruction_raw = raw.split("\n\n")
+
+    crates_transposed = [
+        "".join(line)
+        for line in zip(*crates_raw.split("\n"))
+        if any(char in "123456789" for char in line)
+    ]
+    crates_reversed_order = [list(reversed(stack)) for stack in crates_transposed]
+    crates = [
+        [crate for crate in stack if (crate >= "A" and crate <= "Z")]
+        for stack in crates_reversed_order
+    ]
+
+    instructions = [
+        tuple(map(int, re.findall(r"\d+", line)))
+        for line in instruction_raw.split("\n")
+    ]
+
+    return {"crates": crates, "instructions": instructions}
+
+
+def run_instruction(
+    crates: list[list[str]],
+    num: int,
+    from_stack: int,
+    to_stack: int,
+    part_two: bool = False,
+) -> list[list[str]]:
+    from_stack, to_stack = from_stack - 1, to_stack - 1  # account of 1-base index
+
+    crates_remain, crate_taken = crates[from_stack][:-num], crates[from_stack][-num:]
+    crates[from_stack] = crates_remain
+    if part_two:
+        crates[to_stack] += crate_taken
+    else:
+        crates[to_stack] += reversed(crate_taken)
+
+    return crates
+
+
+def part_one(data) -> str:
+    crates, instructions = data["crates"], data["instructions"]
+    for inst in instructions:
+        crates = run_instruction(crates, *inst)
+
+    return "".join(stack[-1] for stack in crates)
+
+
+def part_two(data):
+    crates, instructions = data["crates"], data["instructions"]
+    for inst in instructions:
+        crates = run_instruction(crates, *inst, part_two=True)
+
+    return "".join(stack[-1] for stack in crates)
+
+
+if __name__ == "__main__":
+
+    day = 5
+
+    raw_data = aoc_helper.fetch(day, 2022)
+    parsed_data = parse_raw(raw_data)
+
+    print(f"part one solution: {part_one(parsed_data)}")
+
+    # parse the data again as part one mutates the crates
+    parsed_data = parse_raw(raw_data)
+    print(f"part two solution: {part_two(parsed_data)}")
---
+++ tests/day_05_test.py
@@ -0,0 +1,82 @@
+from solutions.day_05 import parse_raw, part_one, part_two, run_instruction
+
+
+example = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"""
+
+
+def test_parse_raw():
+    expected = {
+        "crates": [["Z", "N"], ["M", "C", "D"], ["P"]],
+        "instructions": [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)],
+    }
+
+    actual = parse_raw(raw=example)
+
+    assert actual == expected
+
+
+def test_run_instruction():
+    input_crates = [["Z", "N"], ["M", "C", "D"], ["P"]]
+    input_instructions = [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]
+
+    expected_round_2 = [["Z", "N", "D"], ["M", "C"], ["P"]]
+
+    expected_round_3 = [[], ["M", "C"], ["P", "D", "N", "Z"]]
+
+    expected_round_4 = [["C", "M"], [], ["P", "D", "N", "Z"]]
+
+    expected_round_5 = [["C"], ["M"], ["P", "D", "N", "Z"]]
+
+    expected = [expected_round_2, expected_round_3, expected_round_4, expected_round_5]
+
+    crates = input_crates
+    for i, instruction in enumerate(input_instructions):
+        crates = run_instruction(crates, *instruction)
+        assert crates == expected[i]
+
+
+def test_run_instruction_part_two():
+
+    input_crates = [["Z", "N"], ["M", "C", "D"], ["P"]]
+    input_instructions = [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]
+
+    expected_round_2 = [["Z", "N", "D"], ["M", "C"], ["P"]]
+
+    expected_round_3 = [[], ["M", "C"], ["P", "Z", "N", "D"]]
+
+    expected_round_4 = [["M", "C"], [], ["P", "Z", "N", "D"]]
+
+    expected_round_5 = [["M"], ["C"], ["P", "Z", "N", "D"]]
+
+    expected = [expected_round_2, expected_round_3, expected_round_4, expected_round_5]
+
+    crates = input_crates
+    for i, instruction in enumerate(input_instructions):
+        crates = run_instruction(crates, *instruction, part_two=True)
+        assert crates == expected[i]
+
+
+def test_part_one():
+    input_data = parse_raw(example)
+
+    expected = "CMZ"
+    actual = part_one(input_data)
+
+    assert actual == expected
+
+
+def test_part_two():
+    input_data = parse_raw(example)
+
+    expected = "MCD"
+    actual = part_two(input_data)
+
+    assert actual == expected
