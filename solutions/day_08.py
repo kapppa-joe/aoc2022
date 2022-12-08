1eb86dc155d3bfcf74917a4ee5905496b1f3ad5c
kapppa.joe@gmail.com <joe.fong@madetech.com>
2022-12-08 11:51:47 +0000

    add day 08

---
+++ solutions/day_08.py
@@ -0,0 +1,71 @@
+import aoc_helper
+import numpy as np
+import itertools
+
+
+def parse_raw(raw: str) -> np.ndarray:
+    return np.array([[int(digit) for digit in line] for line in raw.split("\n")])
+
+
+def see_from_four_directions(grid: np.ndarray, x: int, y: int) -> list[list[int]]:
+    return [
+        list(arr)
+        for arr in [
+            reversed(grid[0:y, x]),
+            grid[y, x + 1 :],
+            grid[y + 1 :, x],
+            reversed(grid[y, 0:x]),
+        ]
+    ]
+
+
+def visibility_from_four_directions(grid: np.ndarray, x: int, y: int) -> list[bool]:
+    self_height = grid[y][x]
+    return [all(trees < self_height) for trees in see_from_four_directions(grid, x, y)]
+
+
+def is_visible(grid: np.ndarray, x: int, y: int) -> bool:
+    return any(visibility_from_four_directions(grid, x, y))
+
+
+def count_trees_saw(grid: np.ndarray, x: int, y: int) -> list[int]:
+    self_height = grid[y][x]
+    counts = []
+
+    for trees in see_from_four_directions(grid, x, y):
+        not_blocked = list(itertools.takewhile(lambda tree: tree < self_height, trees))
+        blocked = itertools.dropwhile(lambda tree: tree < self_height, trees)
+        seen = sum(1 for _ in not_blocked)
+        seen += 1 if any(tree for tree in blocked) else 0
+        counts.append(seen)
+
+    return counts
+
+
+def part_one(grid: np.ndarray) -> int:
+    return sum(
+        1
+        for y in range(grid.shape[0])
+        for x in range(grid.shape[1])
+        if is_visible(grid, x, y)
+    )
+
+
+def part_two(grid: np.ndarray) -> int:
+
+    return max(
+        np.prod(a=count_trees_saw(grid, x, y), dtype=int)
+        for y in range(grid.shape[0])
+        for x in range(grid.shape[1])
+    )
+
+
+if __name__ == "__main__":
+
+    day = 8
+
+    raw_data = aoc_helper.fetch(day, 2022)
+    parsed_data = parse_raw(raw_data)
+
+    print(f"part one solution: {part_one(parsed_data)}")
+    print(f"part two solution: {part_two(parsed_data)}")
---
+++ tests/day_08_test.py
@@ -0,0 +1,116 @@
+import pytest
+import numpy as np
+
+from solutions.day_08 import (
+    parse_raw,
+    part_one,
+    part_two,
+    see_from_four_directions,
+    visibility_from_four_directions,
+    is_visible,
+    count_trees_saw,
+)
+
+example = """30373
+25512
+65332
+33549
+35390"""
+
+
+def test_parse_raw():
+    expected = np.array(
+        [
+            [3, 0, 3, 7, 3],
+            [2, 5, 5, 1, 2],
+            [6, 5, 3, 3, 2],
+            [3, 3, 5, 4, 9],
+            [3, 5, 3, 9, 0],
+        ]
+    )
+
+    actual = parse_raw(example)
+
+    assert (actual == expected).all()
+
+
+def test_see_from_four_directions():
+    grid = parse_raw(example)
+
+    coord = (2, 1)
+    expected = [[3], [1, 2], [3, 5, 3], [5, 2]]
+    actual = see_from_four_directions(grid, *coord)
+    assert actual == expected
+
+
+def test_visibility_from_four_directions():
+    grid = parse_raw(example)
+
+    test_cases = [
+        [
+            (1, 1),
+            [True, False, False, True],
+        ],  # top-left 5 is visible from the left and top.
+        [
+            (2, 1),
+            [True, True, False, False],
+        ],  # top-middle 5 is visible from the top and right.
+        [(0, 0), [True, False, False, True]],  # top-left 3 is visible from two edges.
+    ]
+
+    for coord, expected in test_cases:
+        actual = visibility_from_four_directions(grid, *coord)
+        assert actual == expected
+
+
+is_visible_test_cases = [
+    [(0, 0), True],
+    [(0, 4), True],
+    [(1, 4), True],
+    [(4, 1), True],
+    [(1, 1), True],
+    [(1, 2), True],
+    [(1, 3), False],
+    [(2, 1), True],
+    [(2, 2), False],
+    [(2, 3), True],
+    [(3, 1), False],
+    [(3, 2), True],
+    [(3, 3), False],
+]
+
+
+@pytest.mark.parametrize("coord, expected", is_visible_test_cases)
+def test_is_visible(coord, expected):
+    grid = parse_raw(example)
+
+    actual = is_visible(grid, *coord)
+    assert actual == expected
+
+
+def test_part_one():
+    grid = parse_raw(example)
+
+    expected = 21
+
+    actual = part_one(grid)
+
+    assert actual == expected
+
+
+def test_count_trees_saw():
+    grid = parse_raw(example)
+
+    coord = (2, 1)
+    expected = [1, 2, 2, 1]
+    actual = count_trees_saw(grid, *coord)
+    assert actual == expected
+
+
+def test_part_two():
+    grid = parse_raw(example)
+
+    expected = 8
+    actual = part_two(grid)
+
+    assert actual == expected
