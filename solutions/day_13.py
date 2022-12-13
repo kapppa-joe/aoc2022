2412f984e12224ebeebbc397bd68c709b8363ab1
kapppa.joe@gmail.com <joe.fong@madetech.com>
2022-12-13 12:33:11 +0000

    add day 13 part 2

--- solutions/day_13.py
+++ solutions/day_13.py
@@ -1,32 +1,52 @@
 from typing import TypeAlias, Union
 from itertools import zip_longest
+from functools import cache, cmp_to_key
 import re
 
 import aoc_helper
 
 Packet: TypeAlias = list[Union[int, "Packet"]]
 
+DividerPackets = ["[[2]]", "[[6]]"]
 
-def parse_raw(raw: str) -> list[tuple[Packet, Packet]]:
+
+def parse_raw(raw: str) -> list[tuple[str, str]]:
     sanitised_raw = re.sub(r"[^\[\] \n\d\,]", "", raw)
-    pairs = [pair.split("\n") for pair in sanitised_raw.split("\n\n")]
-
-    return [(eval(pair[0]), eval(pair[1])) for pair in pairs]
+    return [tuple(pair.split("\n")) for pair in sanitised_raw.split("\n\n")]
 
 
-def in_right_order(left: int | Packet, right: int | Packet) -> bool:
+def add_divider_packets(pairs: list[tuple[str, str]]) -> list[str]:
+
+    return [packet_str for pair in pairs for packet_str in pair] + DividerPackets
+
+
+@cache
+def parse_packet_string(packet_string: str) -> Packet:
+    return eval(packet_string)
+
+
+def in_right_order(left_str: str, right_str: str) -> bool:
+    left, right = parse_packet_string(left_str), parse_packet_string(right_str)
+    return compare_packets(left, right) == -1
+
+
+def compare_packets(left: int | Packet, right: int | Packet) -> int:
     if isinstance(left, int) and isinstance(right, int):
-        return left < right
+        if left == right:
+            return 0
+        return -1 if left < right else 1
+
     elif isinstance(left, list) and isinstance(right, list):
         for left_elem, right_elem in zip_longest(left, right, fillvalue=None):
-            if left_elem == right_elem:
-                continue
-            elif left_elem is None or right_elem is None:
-                return left_elem is None
+            if left_elem is None or right_elem is None:
+                return -1 if left_elem is None else 1
             else:
-                return in_right_order(left_elem, right_elem)
+                curr_element_comparison = compare_packets(left_elem, right_elem)
+                if curr_element_comparison in [-1, 1]:
+                    return curr_element_comparison
+        return 0
     else:
-        return in_right_order(convert_int_to_list(left), convert_int_to_list(right))
+        return compare_packets(convert_int_to_list(left), convert_int_to_list(right))
 
     raise RuntimeError("Got unexpected pattern of input data")
 
@@ -37,16 +57,24 @@ def convert_int_to_list(value: int | list) -> list:
     return value
 
 
-def cmp_packets(left: Packet, right: Packet) -> int:
-    return -1 if in_right_order(left, right) else 1
+@cache
+def cmp_packets_strings(left_str: str, right_str: str) -> int:
+    left, right = parse_packet_string(left_str), parse_packet_string(right_str)
+    return compare_packets(left, right)
 
 
-def part_one(pairs: list[tuple[Packet, Packet]]) -> int:
+def sort_packet_strings(packet_strings: list[str]) -> list[str]:
+    return sorted(packet_strings, key=cmp_to_key(cmp_packets_strings))
+
+
+def part_one(pairs: list[tuple[str, str]]) -> int:
     return sum(i + 1 for i, pair in enumerate(pairs) if in_right_order(*pair))
 
 
-def part_two(data):
-    ...
+def part_two(packet_strings: list[str]):
+    packets_sorted = sort_packet_strings(packet_strings)
+    divider_indices = [packets_sorted.index(divider) + 1 for divider in DividerPackets]
+    return divider_indices[0] * divider_indices[1]
 
 
 if __name__ == "__main__":
@@ -57,4 +85,6 @@ if __name__ == "__main__":
     parsed_data = parse_raw(raw_data)
 
     print(f"part one solution: {part_one(parsed_data)}")
-    print(f"part two solution: {part_two(parsed_data)}")
+
+    packet_strings_with_divider = add_divider_packets(parsed_data)
+    print(f"part two solution: {part_two(packet_strings_with_divider)}")
--- tests/day_13_test.py
+++ tests/day_13_test.py
@@ -1,4 +1,14 @@
-from solutions.day_13 import parse_raw, in_right_order, part_one, part_two
+import pytest
+
+from solutions.day_13 import (
+    parse_raw,
+    parse_packet_string,
+    in_right_order,
+    add_divider_packets,
+    sort_packet_strings,
+    part_one,
+    part_two,
+)
 
 example = """[1,1,3,1,1]
 [1,1,5,1,1]
@@ -25,7 +35,7 @@ example = """[1,1,3,1,1]
 [1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
 
-def test_parse_raw():
+def test_parse_packet_string():
     expected = [
         ([1, 1, 3, 1, 1], [1, 1, 5, 1, 1]),
         ([[1], [2, 3, 4]], [[1], 4]),
@@ -36,9 +46,9 @@ def test_parse_raw():
         ([[[]]], [[]]),
         ([1, [2, [3, [4, [5, 6, 7]]]], 8, 9], [1, [2, [3, [4, [5, 6, 0]]]], 8, 9]),
     ]
-    actual = parse_raw(example)
-
-    assert actual == expected
+    for i, str_pair in enumerate(parse_raw(example)):
+        actual = (parse_packet_string(str_pair[0]), parse_packet_string(str_pair[1]))
+        assert actual == expected[i]
 
 
 def test_in_right_order():
@@ -56,5 +66,75 @@ def test_part_one():
     expected = 13
 
     actual = part_one(pairs)
+
+    assert actual == expected
+
+
+def test_add_divider_packets():
+    pairs = parse_raw(example)
+
+    expected = [
+        "[1,1,3,1,1]",
+        "[1,1,5,1,1]",
+        "[[1],[2,3,4]]",
+        "[[1],4]",
+        "[9]",
+        "[[8,7,6]]",
+        "[[4,4],4,4]",
+        "[[4,4],4,4,4]",
+        "[7,7,7,7]",
+        "[7,7,7]",
+        "[]",
+        "[3]",
+        "[[[]]]",
+        "[[]]",
+        "[1,[2,[3,[4,[5,6,7]]]],8,9]",
+        "[1,[2,[3,[4,[5,6,0]]]],8,9]",
+        "[[2]]",
+        "[[6]]",
+    ]
+
+    actual = add_divider_packets(pairs)
+
+    assert actual == expected
+
+
+@pytest.fixture(name="part_two_packet_strings")
+def make_part_two_input():
+    pairs = parse_raw(example)
+    part_two_packet_strings = add_divider_packets(pairs)
+    yield part_two_packet_strings
+
+
+def test_sort_packet_strings(part_two_packet_strings):
+    expected = """[]
+[[]]
+[[[]]]
+[1,1,3,1,1]
+[1,1,5,1,1]
+[[1],[2,3,4]]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[[1],4]
+[[2]]
+[3]
+[[4,4],4,4]
+[[4,4],4,4,4]
+[[6]]
+[7,7,7]
+[7,7,7,7]
+[[8,7,6]]
+[9]""".split(
+        "\n"
+    )
+
+    actual = sort_packet_strings(part_two_packet_strings)
+
+    assert actual == expected
+
+
+def test_part_two(part_two_packet_strings):
+    expected = 140
+    actual = part_two(part_two_packet_strings)
 
     assert actual == expected
