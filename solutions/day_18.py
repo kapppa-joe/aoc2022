792714038eef5a9beb0f0929b9dc5ae274aa15e7
Joe Fong <kapppa.joe@gmail.com>
2022-12-18 18:16:18 +0000

    save current progress

--- solutions/day_18.py
+++ solutions/day_18.py
@@ -1,8 +1,11 @@
 import itertools, functools, numpy as np
+from typing import Iterable
+from collections import defaultdict
 
 import aoc_helper
 
 Cube = tuple[int, int, int]
+Boundries = tuple[tuple[int, int], tuple[int, int], tuple[int, int]]
 
 
 def parse_raw(raw: str) -> list[Cube]:
@@ -17,16 +20,109 @@ def is_touching(cube_a: Cube, cube_b: Cube) -> bool:
     return sorted(diff_per_axes) == [0, 0, 1]
 
 
-def part_one(cubes: list[Cube]) -> int:
-    possible_pairs = itertools.combinations(cubes, r=2)
-    touching_faces = sum(is_touching(a, b) for a, b in possible_pairs)
-    total_faces = len(cubes) * 6
-
-    return total_faces - touching_faces * 2
+def part_one(lava_cubes: list[Cube]) -> int:
+    return object_surface_area(lava_cubes)
 
 
-def part_two(data):
-    ...
+class DisjointSets:
+    def __init__(self):
+        self.parents = defaultdict(lambda: -1)
+
+    def find(self, cube: Cube) -> Cube:
+        if self.parents[cube] == -1:
+            return cube
+        return self.find(self.parents[cube])
+
+    def union(self, a: Cube, b: Cube):
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a == root_b:
+            return
+        self.parents[root_a] = root_b
+
+    def clean_up():
+
+
+def find_boundries(lava_cubes: list[Cube]) -> Boundries:
+    each_axis = [[cube[i] for cube in lava_cubes] for i in range(3)]
+    return [(min(axis), max(axis)) for axis in each_axis]
+
+
+def is_at_boundry(cube: Cube, boundries: Boundries) -> bool:
+    return any(cube[axis] in boundries[axis] for axis in [0, 1, 2])
+
+
+def all_spaces(boundries: Boundries) -> Iterable[Cube]:
+    as_ranges = map(lambda minmax: range(minmax[0], minmax[1] + 1), boundries)
+    return itertools.product(*as_ranges)
+
+
+def all_touching_pairs(boundries: Boundries) -> Iterable[frozenset[Cube, Cube]]:
+    upper_boundries = [upper for _, upper in boundries]
+    for cube in all_spaces(boundries=boundries):
+        x, y, z = cube
+        if x != upper_boundries[0]:
+            yield frozenset([cube, (x + 1, y, z)])
+        if y != upper_boundries[1]:
+            yield frozenset([cube, (x, y + 1, z)])
+        if z != upper_boundries[2]:
+            yield frozenset([cube, (x, y, z + 1)])
+    return
+
+
+def group_cubes(lava_cubes: list[Cube]) -> dict:
+    boundries = find_boundries(lava_cubes)
+    lava_set = set(lava_cubes)
+    not_lava = lambda cube: cube not in lava_set
+    confirmed_expose = lambda cube: not_lava(cube) and is_at_boundry(
+        cube=cube, boundries=boundries
+    )
+
+    ds = DisjointSets()
+    lava_root = lava_cubes[0]
+    for cube in lava_cubes[1:]:
+        ds.union(cube, lava_root)
+
+    for cube_a, cube_b in all_touching_pairs(boundries=boundries):
+        if not_lava(cube_a) and not_lava(cube_b):
+            ds.union(cube_b, cube_a)
+
+    group_dict = defaultdict(lambda: set())
+    trapped_air_groups = defaultdict(lambda: set())
+    group_dict["trapped_air_groups"] = trapped_air_groups
+
+    confirmed_exposed_cube = next(
+        cube for cube in all_spaces(boundries=boundries) if confirmed_expose(cube)
+    )
+    for cube in all_spaces(boundries=boundries):
+        cube_root = ds.find(cube)
+        if cube_root == lava_root:
+            group_dict["lava_cubes"].add(cube)
+        elif cube_root == ds.find(confirmed_exposed_cube):
+            group_dict["exposed"].add(cube)
+        else:
+            trapped_air_groups[cube_root].add(cube)
+
+    return group_dict
+
+
+def object_surface_area(group: Iterable[Cube]) -> int:
+    possible_pairs = itertools.combinations(group, r=2)
+    touching_pairs = sum(is_touching(a, b) for a, b in possible_pairs)
+    total_faces = len(group) * 6
+
+    return total_faces - touching_pairs * 2
+
+
+def part_two(lava_cubes: list[Cube]) -> int:
+    groups = group_cubes(lava_cubes=lava_cubes)
+    lava_surface = object_surface_area(lava_cubes)
+    trapped_air_groups = groups["trapped_air_groups"].values()
+    deduction = sum(
+        object_surface_area(trapped_air) for trapped_air in trapped_air_groups
+    )
+
+    return lava_surface - deduction
 
 
 if __name__ == "__main__":
--- tests/day_18_test.py
+++ tests/day_18_test.py
@@ -1,4 +1,17 @@
-from solutions.day_18 import parse_raw, is_touching, part_one, part_two
+from collections import defaultdict
+from solutions.day_18 import (
+    parse_raw,
+    is_touching,
+    part_one,
+    DisjointSets,
+    find_boundries,
+    all_spaces,
+    is_at_boundry,
+    group_cubes,
+    all_touching_pairs,
+    object_surface_area,
+    part_two,
+)
 
 example = """2,2,2
 1,2,2
@@ -61,5 +74,136 @@ def test_part_one():
     expected = 64
 
     actual = part_one(cubes)
+
+    assert actual == expected
+
+
+def test_disjoint_sets():
+    # test case:
+    # 2,3,5,7 - one group
+    # 1 - one group
+    # 4,6,8,9,10 - one group
+
+    ds = DisjointSets()
+    ds.union(2, 3)
+    ds.union(3, 5)
+    ds.union(5, 7)
+    ds.union(4, 6)
+    ds.union(6, 9)
+    ds.union(8, 10)
+    ds.union(4, 10)
+    group_dict = defaultdict(lambda: set())
+
+    for i in range(1, 11):
+        root = ds.find(i)
+        group_dict[root].add(i)
+
+    assert len(group_dict) == 3
+
+    groups = list(group_dict.values())
+    assert {2, 3, 5, 7} in groups
+    assert {1} in groups
+    assert {4, 6, 8, 9, 10} in groups
+
+
+def test_find_boundries():
+    lava_cubes = parse_raw(example)
+    expected = [(1, 3), (1, 3), (1, 6)]
+
+    actual = find_boundries(lava_cubes)
+    assert actual == expected
+
+
+def test_all_spaces():
+    lava_cubes = parse_raw(example)
+    boundries = find_boundries(lava_cubes)
+
+    actual = list(all_spaces(boundries=boundries))
+    assert len(actual) == 3 * 3 * 6
+    assert (1, 1, 1) in actual
+    assert (1, 1, 6) in actual
+    assert (3, 3, 6) in actual
+    assert (1, 6, 1) not in actual
+    assert (4, 1, 1) not in actual
+    assert all(cube in actual for cube in lava_cubes)
+
+
+def test_is_at_boundry():
+    # 4 x 4 cube, only 8 pieces in center is not at boundry.
+    test_cases = [
+        (x, y, z) for x in range(1, 5) for y in range(1, 5) for z in range(1, 5)
+    ]
+
+    boundries = [(1, 4), (1, 4), (1, 4)]
+    cubes_in_center = [
+        (2, 2, 2),
+        (2, 2, 3),
+        (2, 3, 2),
+        (2, 3, 3),
+        (3, 2, 2),
+        (3, 2, 3),
+        (3, 3, 2),
+        (3, 3, 3),
+    ]
+    for cube in test_cases:
+        if cube in cubes_in_center:
+            assert is_at_boundry(cube, boundries) == False
+        else:
+            assert is_at_boundry(cube, boundries) == True
+
+
+def test_all_touching_pairs():
+    boundries = ((1, 3), (1, 3), (1, 3))
+    actual = list(all_touching_pairs(boundries=boundries))
+
+    assert len(actual) == (27 * 6 - 9 * 6) // 2
+    # all surface area - outside facing 6 faces * 9 units
+    # for each touching pair, there are two unit of touching surface.
+
+    assert len(set(actual)) == len(actual)
+
+    assert frozenset([(1, 1, 1), (1, 1, 2)]) in actual
+    assert frozenset([(2, 2, 1), (2, 3, 1)]) in actual
+    assert frozenset([(3, 3, 3), (2, 3, 3)]) in actual
+    assert frozenset([(1, 1, 1), (1, 1, 3)]) not in actual
+    assert frozenset([(3, 1, 3), (1, 1, 3)]) not in actual
+    assert frozenset([(1, 1, 1), (1, 2, 2)]) not in actual
+
+    for pairs in actual:
+        assert is_touching(*pairs) == True
+
+
+def test_group_cubes():
+    lava_cubes = parse_raw(example)
+    actual = group_cubes(lava_cubes)
+
+    assert actual["lava_cubes"] == set(lava_cubes)
+
+    assert isinstance(actual["trapped_air_groups"], dict)
+    assert set([(2, 2, 5)]) in actual["trapped_air_groups"].values()
+
+    assert len(actual["exposed"]) == 3 * 3 * 6 - 13 - 1
+
+
+def test_object_surface_area():
+    test_cases = [
+        # fmt: off
+        [set([(1,1,1)]), 6],
+        [set([(1,1,1), (1,1,2), (2,1,1)]), 5 + 5 + 4], # L shape
+        [set([(1,1,1), (1,1,2), (2,1,1), (2,1,2)]), 4 * 4], # square
+        [set([(1,1,1), (1,1,2), (2,1,2), (2,2,2), (3,2,2)]), 5 + 5 + 2 * 6], # W shape
+        [set([(1,1,1), (1,1,2), (1,0,2), (0,1,2), (0,1,3)]), 4 + 4 + 4 + 4 + 3 + 3],
+        # fmt: on
+    ]
+
+    for groups, expected in test_cases:
+        actual = object_surface_area(group=groups)
+        assert actual == expected
+
+
+def test_part_two():
+    lava_cubes = parse_raw(example)
+    expected = 58
+    actual = part_two(lava_cubes)
 
     assert actual == expected
