b546aeff05b2ade0c383b742f0dfed4d320c6d33
Joe Fong <kapppa.joe@gmail.com>
2022-12-19 21:56:00 +0000

    brute forced thru part one. to improve algorithm

--- solutions/day_19.py
+++ solutions/day_19.py
@@ -1,7 +1,7 @@
 import re
 import itertools
 import functools
-from typing import NamedTuple
+from typing import Iterable, NamedTuple
 
 import aoc_helper
 
@@ -14,59 +14,131 @@ class Blueprint(NamedTuple):
     geode_bot_obsidian_cost: int
 
 class State(NamedTuple):
-    ore_bot: int = 1
-    clay_bot: int = 0
-    obsidian_bot: int = 0
     geode_bot: int = 0
-    ore: int = 0
-    clay: int = 0
-    obsidian: int = 0
+    obsidian_bot: int = 0
+    clay_bot: int = 0
+    ore_bot: int = 0
     geode: int = 0
+    obsidian: int = 0
+    clay: int = 0
+    ore: int = 0
 
-    def __add__(self, other: "State"):
+    def __add__(self, other: "State") -> "State":
         if isinstance(other, self.__class__):
             return State(*map(sum, zip(self, other)))
         raise ValueError('can only add state to state')
 
+    def __sub__(self, other: "State") -> "State":
+        deduction = [value * -1 for value in other]
+        if isinstance(other, self.__class__):
+            return State(*map(sum, zip(self, deduction)))
+        raise ValueError('can only subtract state from state')
+
+    def __repr__(self) -> str:
+        fmt = (f"{name}={qty}" for name, qty in self._asdict().items() if qty > 0)
+        return self.__class__.__name__ + f"({', '.join(fmt)})"
+
+    def can_afford(self, other: "State") -> bool:
+        if not isinstance(other, self.__class__):
+            raise ValueError('can only compare state with state')  
+
+        self_resources = self[4:]
+        resources_cost = other[4:]
+        
+        return all(has_got >= cost for has_got, cost in zip(self_resources, resources_cost))
+
+    def better_than(self, other: "State") -> bool:
+        return all(self_qty >= other_qty for self_qty, other_qty in zip(self, other))
+
+BotTypes = State._fields[:4]
+ResourceTypes = State._fields[4:]
 
 
 class RobotFactory():
     def __init__(self, blueprints:list[Blueprint]):
         self.blueprints = blueprints
 
-    # @functools.cache
-    def run_blueprint(self, blueprint_number: int, input_state: State) -> list[State]:
+    @functools.cache
+    def get_bot_cost(self, blueprint_num:int, bot_type:str) -> State:
+        blueprint = self.blueprints[blueprint_num - 1]
+        recipe_dict = {}
+
+        for recipe, qty in blueprint._asdict().items():
+            if recipe.startswith(bot_type):
+                resource_type = recipe.replace(f'{bot_type}_', '').replace('_cost', '')
+                recipe_dict[resource_type] = qty
+
+        return State(**recipe_dict)
+
+    def can_make_bot(self, blueprint_num:int, input_state: State) -> list[str]:
+        result = []
+        for bot_type in BotTypes:
+            cost = self.get_bot_cost(blueprint_num=blueprint_num, bot_type=bot_type)
+            if input_state.can_afford(cost):
+                result.append(bot_type)
+
+        return result
+
+    @functools.cache
+    def run_blueprint(self, blueprint_num: int, input_state: State) -> list[State]:
         bots_at_start = input_state[:4]
         resources_gathered = State(0, 0, 0, 0, *bots_at_start)
 
-        blueprint = self.blueprints[blueprint_number - 1]
-        recipe = ''.join(blueprint._fields)
-
         output_states = []
 
-        if input_state.ore >= blueprint.clay_bot_ore_cost:
-            new_state = input_state._replace(
-                ore=input_state.ore - blueprint.clay_bot_ore_cost,
-                clay_bot=input_state.clay_bot + 1
-            )
+        bot_choices = self.can_make_bot(blueprint_num=blueprint_num, input_state=input_state)
+        for bot_type in bot_choices:
+            cost = self.get_bot_cost(blueprint_num=blueprint_num, bot_type=bot_type)
+            new_state = input_state - cost + State(**{bot_type: 1})
             output_states.append(new_state)
 
-        # if don't make any bot
-        output_states.append(input_state)
+            if bot_type == 'geode_bot':
+                # if can make geode_bot, rush for it!
+                break
+
+
+        # if some type of bot can't be made now, add the choice of withholding resource.
+        if len(bot_choices) != len(BotTypes) and ('geode_bot' not in bot_choices):
+            output_states.append(input_state)
         
+        # add collected resource at turn end
         output_states = [state + resources_gathered for state in output_states]
         
         return output_states
 
-    def bot_cost(self, blueprint_number:int, bot_name:str) -> State:
-        blueprint = self.blueprints[blueprint_number - 1]
-        recipe = ''.join(blueprint._fields)
-        
-    
-        return State()
+    def trim_inferior_state(self, states: set[State]) -> set[State]:
+        if len(states) < 2:
+            return states 
 
-    def can_make_bot(self, blueprint_number:int, input_state: State) -> list[str]:
-        return []
+        results = set()
+        sorted_states = sorted(states, reverse=True)
+        current = sorted_states[0]
+
+        for state in sorted_states[1:]:
+            if current.better_than(state):
+                continue
+            else:
+                results.add(current)
+                current = state
+        results.add(current)
+
+        return results
+
+    def run_n_turns(self, turn_count: int, blueprint_num: int, initial_state: State) -> set[State]:
+        all_states = set([initial_state])
+        for _ in range(turn_count):
+            all_states = set(output 
+                for state in all_states
+                for output in self.run_blueprint(blueprint_num=blueprint_num, input_state=state) 
+            )
+            all_states = self.trim_inferior_state(all_states)
+        return all_states
+
+    def best_geode_for_blueprint(self, blueprint_num: int, minutes=24) -> int:
+        initial_state = State(ore_bot=1)
+        all_states_collected = self.run_n_turns(turn_count=minutes, blueprint_num=blueprint_num, initial_state=initial_state)
+        return max(state.geode for state in all_states_collected)
+
 
 def parse_raw(raw: str) -> RobotFactory:
     numbers_each_line = (list(map(int, re.findall(r'\d+', line))) for line in raw.splitlines())
@@ -77,8 +149,11 @@ def parse_raw(raw: str) -> RobotFactory:
 
 
 
-def part_one(data):
-    ...
+def part_one(factory: RobotFactory) -> int:
+    return sum(
+        blueprint_num * factory.best_geode_for_blueprint(blueprint_num)
+        for blueprint_num in range(1, len(factory.blueprints) + 1)
+    )
 
 
 def part_two(data):
@@ -87,7 +162,7 @@ def part_two(data):
 
 if __name__ == "__main__":
 
-    day = None
+    day = 19
 
     raw_data = aoc_helper.fetch(day, 2022)
     parsed_data = parse_raw(raw_data)
--- tests/day_19_test.py
+++ tests/day_19_test.py
@@ -21,11 +21,11 @@ def create_example_factory():
 
 
 def test_run_blueprint_1_turn_1(factory):
-    input_state = State()
+    input_state = State(ore_bot=1)
     expected = [
         State(ore_bot=1, ore=1)
     ]
-    actual = factory.run_blueprint(blueprint_number=1, input_state=input_state)
+    actual = factory.run_blueprint(blueprint_num=1, input_state=input_state)
     
     assert actual == expected
 
@@ -35,7 +35,7 @@ def test_run_blueprint_1_turn_2(factory):
     expected = [
         State(ore_bot=1, ore=2)
     ]
-    actual = factory.run_blueprint(blueprint_number=1, input_state=input_state)
+    actual = factory.run_blueprint(blueprint_num=1, input_state=input_state)
     
     assert actual == expected
 
@@ -46,12 +46,37 @@ def test_run_blueprint_1_turn_3(factory):
         State(ore_bot=1, clay_bot=1, ore=1),
         State(ore_bot=1, ore=3)
     ]
-    actual = factory.run_blueprint(blueprint_number=1, input_state=input_state)
+    actual = factory.run_blueprint(blueprint_num=1, input_state=input_state)
     
     assert sorted(actual) == sorted(expected)
 
 
-def test_bot_cost(factory):
+def test_run_blueprint_1_turn_4(factory):
+    input_state = State(ore_bot=1, clay_bot=1, ore=1)
+    expected = [
+        State(ore_bot=1, clay_bot=1, ore=2, clay=1),
+    ]
+    actual = factory.run_blueprint(blueprint_num=1, input_state=input_state)
+    
+    assert sorted(actual) == sorted(expected)
+
+
+
+def test_run_blueprint_1_turn_5(factory):
+    input_state = State(ore_bot=1, clay_bot=1, ore=2, clay=1)
+    expected = [
+        State(ore_bot=1, clay_bot=1, ore=3, clay=2),
+        State(ore_bot=1, clay_bot=2, ore=1, clay=2),
+    ]
+    actual = factory.run_blueprint(blueprint_num=1, input_state=input_state)
+    
+    assert sorted(actual) == sorted(expected)
+
+
+
+
+
+def test_get_bot_cost(factory):
     test_cases_blueprint_1 = [
         ['clay_bot', State(ore=2)],
         ['ore_bot', State(ore=4)],
@@ -59,23 +84,106 @@ def test_bot_cost(factory):
         ['geode_bot', State(ore=2, obsidian=7)],
     ]
 
-    for bot_name, expected in test_cases_blueprint_1:
-        actual = factory.bot_cost(1, bot_name)
+    for bot_type, expected in test_cases_blueprint_1:
+        actual = factory.get_bot_cost(1, bot_type)
+        print(actual)
+        print(expected)
         assert actual == expected
 
 def test_can_make_bot(factory):
 
     test_cases_blueprint_1 = [
-        [State(0,0,0,0,0,0,0,0), ['clay_bot']],
-        [State(0,0,0,0,2,0,0,0), ['clay_bot']],
-        [State(0,0,0,0,4,0,0,0), ['ore_bot','clay_bot']],
-        [State(0,0,0,0,4,14,0,0), ['ore_bot','clay_bot']],
-        [State(0,0,0,0,4,14,0,0), ['ore_bot','clay_bot', 'obsidian_bot']],
-        [State(0,0,0,0,3,14,0,0), ['clay_bot', 'obsidian_bot']],
-        [State(0,0,0,0,4,14,7,0), ['ore_bot','clay_bot', 'obsidian_bot', 'geode_bot']],
-        [State(0,0,0,0,10,0,10,0), ['ore_bot','clay_bot', 'geode_bot']],
+        [State(0,0,0,0,0,0,0,0), []],
+        [State(ore=2), ['clay_bot']],
+        [State(ore=4), ['ore_bot','clay_bot']],
+        [State(ore=5, clay=13), ['ore_bot','clay_bot']],
+        [State(ore=4, clay=14), ['ore_bot','clay_bot', 'obsidian_bot']],
+        [State(ore=3, clay=14), ['clay_bot', 'obsidian_bot']],
+        [State(ore=4, clay=14, obsidian=7), ['ore_bot','clay_bot', 'obsidian_bot', 'geode_bot']],
+        [State(ore=10, obsidian=10), ['ore_bot','clay_bot', 'geode_bot']],
     ]
 
     for input_state, expected in test_cases_blueprint_1:
         actual = factory.can_make_bot(1, input_state)
-        assert actual == expected
+        assert sorted(actual) == sorted(expected)
+
+
+
+def test_trim_inferior_state(factory):
+    input_sets = {
+        State(ore_bot=2, clay_bot=0, obsidian_bot=0, geode_bot=0, ore=1, clay=0, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=2, obsidian_bot=0, geode_bot=0, ore=1, clay=2, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=2, obsidian_bot=0, geode_bot=0, ore=1, clay=1, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=1, obsidian_bot=0, geode_bot=0, ore=3, clay=2, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=1, obsidian_bot=0, geode_bot=0, ore=3, clay=1, obsidian=0, geode=0),
+        State(ore_bot=1, clay_bot=1, obsidian_bot=0, geode_bot=0, ore=3, clay=0, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=0, obsidian_bot=0, geode_bot=0, ore=5, clay=0, obsidian=0, geode=0), 
+    }
+    
+    expected = {
+        State(ore_bot=2, clay_bot=0, obsidian_bot=0, geode_bot=0, ore=1, clay=0, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=2, obsidian_bot=0, geode_bot=0, ore=1, clay=2, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=1, obsidian_bot=0, geode_bot=0, ore=3, clay=2, obsidian=0, geode=0), 
+        State(ore_bot=1, clay_bot=0, obsidian_bot=0, geode_bot=0, ore=5, clay=0, obsidian=0, geode=0), 
+    }
+
+    actual = factory.trim_inferior_state(input_sets)
+
+    assert actual == expected
+
+
+def test_run_n_turns_basic(factory):
+    input_state = State(ore_bot=1)
+    expected = set([
+        State(ore_bot=1, clay_bot=1, ore=3, clay=2),
+        State(ore_bot=1, clay_bot=2, ore=1, clay=2),
+        State(ore_bot=1, clay_bot=0, ore=5, clay=0),
+        State(ore_bot=2, clay_bot=0, ore=1, clay=0),
+    ])
+
+    actual = factory.run_n_turns(turn_count=5, blueprint_num=1, initial_state=input_state)
+
+    assert actual == expected
+
+
+
+# def test_run_n_turns_example_run_down(factory):
+#     example_rundown = [
+#         # [5, State(ore_bot=1, clay_bot=2, ore=1, clay=2)],
+#         # [6, State(ore_bot=1, clay_bot=2, ore=2, clay=4)],
+#         # [7, State(ore_bot=1, clay_bot=3, ore=1, clay=6)],
+#         # [8, State(ore_bot=1, clay_bot=3, ore=2, clay=9)],
+#         # [9, State(ore_bot=1, clay_bot=3, ore=3, clay=12)],
+#         # [10, State(ore_bot=1, clay_bot=3, ore=4, clay=15)],
+#         # [11, State(ore_bot=1, clay_bot=3, obsidian_bot=1, ore=2, clay=4)],
+#         # [12, State(ore_bot=1, clay_bot=4, obsidian_bot=1, ore=1, clay=7, obsidian=1)],
+#         # [13, State(ore_bot=1, clay_bot=4, obsidian_bot=1, ore=2, clay=11, obsidian=2)],
+#         # [14, State(ore_bot=1, clay_bot=4, obsidian_bot=1, ore=3, clay=15, obsidian=3)],
+#         # [15, State(ore_bot=1, clay_bot=4, obsidian_bot=2, ore=1, clay=5, obsidian=4)],
+#         # [16, State(ore_bot=1, clay_bot=4, obsidian_bot=2, ore=2, clay=9, obsidian=6)],
+#         # [17, State(ore_bot=1, clay_bot=4, obsidian_bot=2, ore=3, clay=13, obsidian=8)],
+#         # [18, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=1, ore=2, clay=17, obsidian=3)],
+#         # [19, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=1, ore=3, clay=21, obsidian=5, geode=1)],
+#         # [20, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=1, ore=4, clay=25, obsidian=7, geode=2)],
+#         # [21, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=2, ore=3, clay=29, obsidian=2, geode=3)],
+#         # [22, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=2, ore=4, clay=33, obsidian=4, geode=5)],
+#         # [23, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=2, ore=5, clay=37, obsidian=6, geode=7)],
+#         [24, State(ore_bot=1, clay_bot=4, obsidian_bot=2, geode_bot=2, ore=6, clay=41, obsidian=8, geode=9)],
+#     ]
+#     initial_state = State(ore_bot=1)
+
+#     for turn_count, expected_state in example_rundown:
+#         actual = factory.run_n_turns(turn_count=turn_count, blueprint_num=1, initial_state=initial_state)
+#         assert expected_state in actual
+
+# def test_best_geode_for_blueprint(factory):
+#     expected = 12
+#     actual = factory.best_geode_for_blueprint(blueprint_num=2)
+
+#     assert actual == expected
+
+def test_part_one(factory):
+    expected = 33
+    actual = part_one(factory)
+
+    assert actual == expected
