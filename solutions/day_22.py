d6b972ce25479b2bb6249938a1448bed3b7dc5a3
Joe Fong <kapppa.joe@gmail.com>
2023-01-31 21:15:10 +0000

    add day 22 part two

--- solutions/day_22.py
+++ solutions/day_22.py
@@ -1,10 +1,18 @@
+import functools
+import itertools
 import re
 from enum import Enum
-from typing import NamedTuple, Optional
+from typing import Optional
 
 
 class Facing(Enum):
     Right, Down, Left, Up = range(4)
+
+    def __sub__(self, other: "Facing") -> int:
+        return (self.value - other.value + 4) % 4
+
+    def turn_clockwise(self, times: int = 1) -> "Facing":
+        return self.__class__((self.value + times) % 4)
 
 
 class Tile(str, Enum):
@@ -38,7 +46,7 @@ class MonkeyMap:
     def get_tile(self, x: int, y: int) -> Tile:
         return Tile(self.map[y][x])
 
-    def get_init_position(self) -> State:
+    def get_init_state(self) -> State:
         try:
             open_tile_x = self.map[0].index(Tile.Space)
         except ValueError:
@@ -46,8 +54,8 @@ class MonkeyMap:
 
         return (open_tile_x, 0, Facing(0))
 
-    def next_tile(self, position: State) -> tuple[int, int]:
-        x, y, facing = position
+    def next_tile(self, state: State) -> State:
+        x, y, facing = state
 
         delta = {
             Facing.Right: (1, 0),
@@ -64,10 +72,10 @@ class MonkeyMap:
             x1 = (x1 + dx) % self.map_width
             y1 = (y1 + dy) % self.map_height
 
-        return (x1, y1)
+        return (x1, y1, facing)
 
-    def follow_path(self, path: Path, init_pos: Optional[State] = None) -> State:
-        x, y, facing = self.get_init_position() if init_pos is None else init_pos
+    def follow_path(self, path: Path, init_state: Optional[State] = None) -> State:
+        x, y, facing = self.get_init_state() if init_state is None else init_state
 
         for step in path:
             match step:
@@ -77,26 +85,190 @@ class MonkeyMap:
                     facing = Facing((facing.value + 1) % 4)
                 case step if isinstance(step, int):
                     for _ in range(step):
-                        x1, y1 = self.next_tile((x, y, facing))
+                        x1, y1, facing1 = self.next_tile((x, y, facing))
                         if self.get_tile(x=x1, y=y1) == Tile.Block:
                             break
-                        x, y = x1, y1
+                        x, y, facing = x1, y1, facing1
 
         return (x, y, facing)
 
-    def calc_password(self, position: State) -> int:
-        x, y, facing = position
+    def calc_password(self, state: State) -> int:
+        x, y, facing = state
         return (y + 1) * 1000 + (x + 1) * 4 + facing.value
 
 
-def parse_raw(raw: str) -> MonkeyMap:
+class MonkeyMapCube(MonkeyMap):
+    def __init__(self, input_map: list[str], path: list[Step]):
+        super().__init__(input_map=input_map, path=path)
+
+        self.side_width = self.detect_side_width()
+        self.faces = self.detect_faces()
+        self.connect_faces()
+
+    def detect_side_width(self) -> int:
+        total_area = sum(
+            1 for tile in "".join(self.map) if tile in (Tile.Block, Tile.Space)
+        )
+
+        sqrt_of_one_face = (total_area / 6) ** 0.5
+        if sqrt_of_one_face**2 != (total_area // 6):
+            raise ValueError(
+                "Cannot parse cube face. Each side of the cube doesn't seem to be an integer"
+            )
+
+        return int(sqrt_of_one_face)
+
+    def detect_faces(self) -> list[tuple[range, range]]:
+        top_left_corners_of_faces = [
+            (x, y)
+            for y in range(0, self.map_height, self.side_width)
+            for x in range(0, self.map_width, self.side_width)
+        ]
+        faces = [
+            (range(x, x + self.side_width), range(y, y + self.side_width))
+            for x, y in top_left_corners_of_faces
+            if self.get_tile(x=x, y=y) in (Tile.Block, Tile.Space)
+        ]
+        if len(faces) != 6:
+            raise ValueError("Fail to detect the 6 faces of cube map.")
+
+        return faces
+
+    def connect_faces(self):
+        """Establish the connections between faces."""
+        connections = {}
+        # add pairs of direct touching faces
+        for a, b in itertools.combinations(range(6), r=2):
+            x_range_a, y_range_a = self.faces[a]
+            x_range_b, y_range_b = self.faces[b]
+            if x_range_a != x_range_b and y_range_a != y_range_b:
+                # return early if not directly touching
+                continue
+            elif (x_range_a == x_range_b) and (y_range_a.stop == y_range_b.start):
+                connections[(a, Facing.Down)] = (b, Facing.Up)
+                connections[(b, Facing.Up)] = (a, Facing.Down)
+            elif (y_range_a == y_range_b) and (x_range_a.stop == x_range_b.start):
+                connections[(a, Facing.Right)] = (b, Facing.Left)
+                connections[(b, Facing.Left)] = (a, Facing.Right)
+
+        # connect common neighbour faces until all faces are connected
+        while len(connections) < 24:
+            connections = self.connect_common_neighbours(connections)
+
+        self.face_connections = connections
+
+    def connect_common_neighbours(self, connections: dict) -> dict:
+        """Resolve connection from common neighbours.
+        If face a is touching face b, a & b must shares two common neighbours.
+        Find those indirect connections and return as a dict"""
+        new_connections = {}
+
+        for (face_a, a_to_b_direction), (
+            face_b,
+            b_to_a_direction,
+        ) in connections.items():
+            a_b_direction_diff = (b_to_a_direction - a_to_b_direction) + 2
+            # rotate +2 will be in opposite direction of a -> b, so only try +1 and +3
+            for addition_rotation in (+1, +3):
+
+                a_to_c_direction = a_to_b_direction.turn_clockwise(addition_rotation)
+                # return early if already got a -> c
+                if (face_a, a_to_c_direction) in connections:
+                    continue
+
+                b_to_c_direction = a_to_b_direction.turn_clockwise(
+                    a_b_direction_diff + addition_rotation
+                )
+                # skip if b -> c is not established yet
+                if (face_b, b_to_c_direction) not in connections:
+                    continue
+
+                face_c, c_to_b_direction = connections[face_b, b_to_c_direction]
+                a_c_direction_diff = b_to_c_direction - b_to_a_direction
+                c_to_a_direction = c_to_b_direction.turn_clockwise(a_c_direction_diff)
+                new_connections[face_a, a_to_c_direction] = (face_c, c_to_a_direction)
+
+        return {**connections, **new_connections}
+
+    def get_neighbour_face(
+        self, from_face: int, direction: Facing
+    ) -> tuple[int, Facing]:
+        return self.face_connections[(from_face, direction)]
+
+    @functools.cache
+    def get_face_index(self, x: int, y: int) -> int:
+        for face_index, face in enumerate(self.faces):
+            x_range, y_range = face
+            if x in x_range and y in y_range:
+                return face_index
+
+        raise ValueError(f"cannot find position {x, y} on any face")
+
+    def relative_position_on_face(self, x: int, y: int) -> tuple[int, int]:
+        face_index = self.get_face_index(x=x, y=y)
+        x_range, y_range = self.faces[face_index]
+        rel_x = x - x_range.start
+        rel_y = y - y_range.start
+
+        return (rel_x, rel_y)
+
+    def face_position_to_absolute_position(
+        self, face_index: int, rel_x: int, rel_y: int
+    ) -> tuple[int, int]:
+        if rel_x > self.side_width or rel_y > self.side_width:
+            raise ValueError("Got invalid relative position")
+        x_range, y_range = self.faces[face_index]
+        x, y = x_range.start + rel_x, y_range.start + rel_y
+        return x, y
+
+    def next_tile(self, state: State) -> State:
+        x, y, facing = state
+
+        delta = {
+            Facing.Right: (1, 0),
+            Facing.Down: (0, 1),
+            Facing.Left: (-1, 0),
+            Facing.Up: (0, -1),
+        }
+        dx, dy = delta[facing]
+
+        x1 = (x + dx) % self.map_width
+        y1 = (y + dy) % self.map_height
+
+        # handle special case if facing a void tile, or next tile is on another face
+        if self.get_tile(x=x1, y=y1) == Tile.Void or self.get_face_index(
+            x, y
+        ) != self.get_face_index(x1, y1):
+            rel_x, rel_y = self.relative_position_on_face(x, y)
+            rel_x1, rel_y1 = (rel_x + dx) % self.side_width, (
+                rel_y + dy
+            ) % self.side_width
+
+            current_face = self.get_face_index(x, y)
+            dest_face_index, dest_facing = self.get_neighbour_face(current_face, facing)
+            rotation = (dest_facing - facing + 2) % 4
+            for _ in range(rotation):
+                rel_x1, rel_y1 = (self.side_width - rel_y1 - 1, rel_x1)
+
+            x1, y1 = self.face_position_to_absolute_position(
+                dest_face_index, rel_x1, rel_y1
+            )
+            facing = facing.turn_clockwise(rotation)
+
+        return (x1, y1, facing)
+
+
+def parse_raw(raw: str, as_cube: bool = False) -> MonkeyMap | MonkeyMapCube:
     raw_map, raw_path = raw.split("\n\n")
     max_y = max(len(line) for line in raw_map.splitlines())
     input_map = [line.ljust(max_y) for line in raw_map.splitlines()]
 
     path = parse_path(raw_path)
 
-    return MonkeyMap(input_map, path)
+    if not as_cube:
+        return MonkeyMap(input_map, path)
+    else:
+        return MonkeyMapCube(input_map, path)
 
 
 def parse_path(raw_path: str) -> Path:
@@ -110,13 +282,9 @@ def parse_path(raw_path: str) -> Path:
 
 def part_one(monkey_map: MonkeyMap) -> int:
     path = monkey_map.path
-    final_position = monkey_map.follow_path(path)
+    final_state = monkey_map.follow_path(path)
 
-    return monkey_map.calc_password(position=final_position)
-
-
-def part_two(data):
-    ...
+    return monkey_map.calc_password(state=final_state)
 
 
 if __name__ == "__main__":
@@ -124,7 +292,9 @@ if __name__ == "__main__":
     day = 22
 
     raw_data = open(f"puzzle/day_{day}.txt", "r").read()
-    parsed_data = parse_raw(raw_data)
 
+    parsed_data = parse_raw(raw_data)
     print(f"part one solution: {part_one(parsed_data)}")
-    print(f"part two solution: {part_two(parsed_data)}")
+
+    parsed_data_part_two = parse_raw(raw_data, as_cube=True)
+    print(f"part two solution: {part_one(parsed_data_part_two)}")
--- tests/day_22_test.py
+++ tests/day_22_test.py
@@ -1,13 +1,6 @@
 import pytest
 
-from solutions.day_22 import (
-    Facing,
-    MonkeyMap,
-    parse_path,
-    parse_raw,
-    part_one,
-    part_two,
-)
+from solutions.day_22 import Facing, MonkeyMapCube, parse_path, parse_raw, part_one
 
 example = """        ...#
         .#..
@@ -38,8 +31,8 @@ def make_monkey_map():
     yield parse_raw(example)
 
 
-def test_get_init_position(monkey_map):
-    actual = monkey_map.get_init_position()
+def test_get_init_state(monkey_map):
+    actual = monkey_map.get_init_state()
     expected = (8, 0, Facing.Right)
 
     assert expected == actual
@@ -49,10 +42,10 @@ def test_next_tile(monkey_map):
     x, y = (15, 8)
 
     test_cases = [
-        (Facing.Down, (15, 9)),
-        (Facing.Left, (14, 8)),
-        (Facing.Right, (8, 8)),
-        (Facing.Up, (15, 11)),
+        (Facing.Down, (15, 9, Facing.Down)),
+        (Facing.Left, (14, 8, Facing.Left)),
+        (Facing.Right, (8, 8, Facing.Right)),
+        (Facing.Up, (15, 11, Facing.Up)),
     ]
 
     for facing, expected in test_cases:
@@ -60,12 +53,12 @@ def test_next_tile(monkey_map):
         assert actual == expected
 
     example_cases = [
-        ((11, 6, Facing.Right), (0, 6)),  # example case point A -> B
-        ((5, 7, Facing.Down), (5, 4)),  # example case point C -> D
+        ((11, 6, Facing.Right), (0, 6, Facing.Right)),  # example case point A -> B
+        ((5, 7, Facing.Down), (5, 4, Facing.Down)),  # example case point C -> D
     ]
 
-    for position, expected in example_cases:
-        actual = monkey_map.next_tile(position)
+    for state, expected in example_cases:
+        actual = monkey_map.next_tile(state)
         assert actual == expected
 
 
@@ -83,9 +76,9 @@ def test_follow_path(monkey_map):
 
     for (raw_path, expected) in test_cases:
         input_path = parse_path(raw_path)
-        init_position = (8, 0, Facing.Right)
+        init_state = (8, 0, Facing.Right)
 
-        actual = monkey_map.follow_path(path=input_path, init_pos=init_position)
+        actual = monkey_map.follow_path(path=input_path, init_state=init_state)
 
         assert expected == actual
 
@@ -100,5 +93,99 @@ def test_calc_password(monkey_map):
 def test_part_one(monkey_map):
     expected = 6032
     actual = part_one(monkey_map)
+
+    assert actual == expected
+
+
+def test_parse_cube():
+    actual = parse_raw(example, as_cube=True)
+
+    assert isinstance(actual, MonkeyMapCube)
+    assert len(actual.faces) == 6
+    assert actual.side_width == 4
+
+    assert actual.faces[0] == (range(8, 12), range(0, 4))
+    assert actual.faces[1] == (range(0, 4), range(4, 8))
+    assert actual.faces[2] == (range(4, 8), range(4, 8))
+    assert actual.faces[3] == (range(8, 12), range(4, 8))
+    assert actual.faces[4] == (range(8, 12), range(8, 12))
+    assert actual.faces[5] == (range(12, 16), range(8, 12))
+
+
+@pytest.fixture(name="monkey_map_cube")
+def make_monkey_map_cube():
+    cube = parse_raw(example, as_cube=True)
+    if not isinstance(cube, MonkeyMapCube):
+        raise RuntimeError
+    yield cube
+
+
+def test_cube_get_neighbour_face(monkey_map_cube):
+    actual = monkey_map_cube.get_neighbour_face(from_face=0, direction=Facing.Left)
+    expected = (2, Facing.Up)
+
+    assert actual == expected
+
+
+def test_next_tile_for_cube(monkey_map_cube):
+    test_cases = [
+        [(11, 5, Facing.Right), (14, 8, Facing.Down)],
+        [(10, 11, Facing.Down), (1, 7, Facing.Up)],
+    ]
+
+    for init_state, expected in test_cases:
+        actual = monkey_map_cube.next_tile(state=init_state)
+        assert actual == expected
+
+
+@pytest.fixture(name="simple_cube")
+def make_simple_cube():
+    raw_input = """  ....
+  ....
+  ..
+  ..
+  ..
+  ..
+....
+....
+
+1L1R"""
+    yield parse_raw(raw_input, as_cube=True)
+
+
+def test_next_tile_for_another_cube_config(simple_cube):
+
+    test_cases = [
+        [(2, 0, Facing.Up), (2, 7, Facing.Up)],
+        [(2, 0, Facing.Left), (1, 7, Facing.Up)],
+        [(2, 0, Facing.Down), (2, 1, Facing.Down)],
+        [(4, 1, Facing.Down), (3, 2, Facing.Left)],
+        [(3, 5, Facing.Right), (5, 0, Facing.Left)],
+        [(3, 7, Facing.Right), (4, 0, Facing.Down)],
+        [(3, 7, Facing.Down), (3, 0, Facing.Down)],
+    ]
+    for init_state, expected in test_cases:
+        actual = simple_cube.next_tile(state=init_state)
+        assert actual == expected
+
+
+def test_follow_path_for_cube(monkey_map_cube):
+    test_cases = [
+        ("10R5L5", (14, 10, Facing.Down)),
+        ("10R5L5R", (14, 10, Facing.Left)),
+        ("10R5L5R10L4R5L5", (6, 4, Facing.Up)),
+    ]
+
+    for (raw_path, expected) in test_cases:
+        input_path = parse_path(raw_path)
+
+        actual = monkey_map_cube.follow_path(path=input_path)
+
+        assert actual == expected
+
+
+def test_part_two(monkey_map_cube):
+    expected = 5031
+    actual = part_one(monkey_map_cube)
 
     assert actual == expected
