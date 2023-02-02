92c6a81d56e386201772b95e73e0601a40cffd31
Joe Fong <kapppa.joe@gmail.com>
2023-02-02 15:02:23 +0000

    add day 24 part one and two

--- solutions/day_24.py
+++ solutions/day_24.py
@@ -1,26 +1,7 @@
-import numpy as np
 import functools
 from enum import Enum
-from typing import NamedTuple
 
-
-class Coord(NamedTuple):
-    x: int
-    y: int
-
-    def __add__(self, other: tuple[int, int]) -> "Coord":
-        """helper method for easy vector summing
-        >>> Coord(1, 3) + Coord(2, 3)
-        Coord(3, 6)
-        """
-        if isinstance(other, tuple) and len(other) == 2:
-            dx, dy = other
-            return Coord(self.x + dx, self.y + dy)
-        else:
-            raise ValueError("Can only add a coordinate to a pair of int")
-
-    def __repr__(self):
-        return f"Coord({self.x}, {self.y})"
+import numpy as np
 
 
 class Tile(Enum):
@@ -30,11 +11,12 @@ class Tile(Enum):
     BlizzardRight = 4
     BlizzardDown = 8
     BlizzardLeft = 16
+    Expedition = 32
 
     @classmethod
     @functools.cache
     def string_representations(cls) -> dict["Tile", str]:
-        all_tiles = ".#^>v<"
+        all_tiles = ".#^>v<E"
         return {tile: all_tiles[i] for i, tile in enumerate(cls)}
 
     @classmethod
@@ -74,27 +56,30 @@ ObstacleMaps = dict[Tile, np.ndarray]
 
 
 class BlizzardBasin:
-    def __init__(self, input_map: str):
-        lines = input_map.splitlines()
-        combined_map = np.array(
-            [[self.object_index(tile) for tile in line] for line in lines], dtype=int
-        )
-        self.obstacle_maps = {
-            obstacle: (combined_map == obstacle.value) for obstacle in Tile.obstacles()
-        }
+    def __init__(self, raw_string: str):
+        combined_map = self.string_to_combined_map(raw_string)
         self.height, self.width = combined_map.shape
         self.inner_height = self.height - 2
         self.inner_width = self.width - 2
 
-    def object_index(self, tile: str) -> int:
-        return Tile.from_str(tile).value
+        empty_tiles = np.argwhere(combined_map == Tile.Empty.value)
+        self.start = tuple(empty_tiles[0])
+        self.goal = tuple(empty_tiles[-1])
 
-    def obstacle_map_as_string(self, obstacle_maps: ObstacleMaps) -> str:
-        combined_map: np.ndarray = sum(
-            individual_map * obstacle.value
-            for obstacle, individual_map in obstacle_maps.items()
-        )  # pyright: ignore
+        self.obstacle_maps = {
+            obstacle: (combined_map == obstacle.value) for obstacle in Tile.obstacles()
+        }
 
+    def string_to_combined_map(self, raw_string: str) -> np.ndarray:
+        """Convert the string representation as in AoC official site example to a combined map of np.array .
+        Seperate this from __init__ for reuse in tests."""
+        lines = raw_string.splitlines()
+        return np.array(
+            [[Tile.from_str(tile).value for tile in line] for line in lines], dtype=int
+        )
+
+    def combine_map_to_string(self, combined_map: np.ndarray) -> str:
+        """Convert a combined map to the string representation as in AoC official site example. Mostly for testing purpose."""
         converted_to_strings = [
             [Tile.to_str(tile_value) for tile_value in row]
             for row in combined_map.tolist()
@@ -102,55 +87,159 @@ class BlizzardBasin:
         lines = ["".join(row) for row in converted_to_strings]
         return "\n".join(lines)
 
-    def obstacles_for_turn_n(self, n: int) -> ObstacleMaps:
-        dy = n % self.inner_height
-        dx = n % self.inner_width
+    def obstacle_map_as_string(self, obstacle_maps: ObstacleMaps) -> str:
+        """Convert obstacle_maps to the string representation as in AoC official site example. Mostly for testing purpose."""
+        combined_map: np.ndarray = sum(
+            individual_map * obstacle.value
+            for obstacle, individual_map in obstacle_maps.items()
+        )  # pyright: ignore
 
+        return self.combine_map_to_string(combined_map)
+
+    @functools.cache
+    def forecast_blizzard_for_turn_n(self, n: int, blizzard_type: Tile) -> np.ndarray:
+        """Return the locations of certain blizzard on turn N.
+        If we only consider the inner area (those cells within the walls),
+        for blizzard moving left/right, it is just slicing the 0:n columns and concat it to the left or right side of the n: columns.
+        Similar for blizzard moving up/down.
+
+        As the pattern repeats in vertical directions when n >= inner_height and in horizontal directions for n >= inner_width,
+        use functools cache to memoize patterns that already computed before.
+
+        Args:
+            n (int): turn number
+            blizzard_type (Tile): blizzard type
+
+        Returns:
+            np.ndarray: A numpy array of boolean value. True denotes that blizzard exist on that tile.
+        """
+        # fmt: off
+        if n >= self.inner_height and blizzard_type in (Tile.BlizzardUp, Tile.BlizzardDown):
+            return self.forecast_blizzard_for_turn_n(n % self.inner_height, blizzard_type)
+        elif n >= self.inner_width and blizzard_type in (Tile.BlizzardLeft, Tile.BlizzardRight):
+            return self.forecast_blizzard_for_turn_n(n % self.inner_width, blizzard_type)
+        
+        base_map = self.obstacle_maps[blizzard_type]
+        inner_area = base_map[1:-1, 1:-1]
+
+  
+        match blizzard_type:
+            case Tile.BlizzardUp:
+                new_inner_area = np.concatenate([inner_area[n:, :], inner_area[:n, :]], axis=0)
+            case Tile.BlizzardRight:
+                new_inner_area = np.concatenate([inner_area[:, (-1 * n) :], inner_area[:, : (-1 * n)]], axis=1)
+            case Tile.BlizzardDown:
+                new_inner_area = np.concatenate([inner_area[(-1 * n) :, :], inner_area[: (-1 * n), :]], axis=0)
+            case Tile.BlizzardLeft:
+                new_inner_area = np.concatenate([inner_area[:, n:], inner_area[:, :n]], axis=1)
+            case _:
+                raise RuntimeError("Unexpected case. Possibly a bug")
+        # fmt: on
+
+        new_map = np.zeros(shape=base_map.shape, dtype=bool)
+        new_map[1:-1, 1:-1] = new_inner_area
+
+        return new_map
+
+    def make_obstacle_maps_for_turn_n(self, n: int) -> ObstacleMaps:
+        """Return a dict that contains the map of each type of obstacle in turn N
+
+        Args:
+            n (int): turn number
+
+        Returns:
+            ObstacleMaps: A dict that represent the locations of each type of obstacle.
+            Keys are Tile and values are numpy arrays (in bool)
+        """
         new_maps = {}
         new_maps[Tile.Wall] = self.obstacle_maps[Tile.Wall]
 
         for blizzard_type in Tile.blizzards():
-            base_map = self.obstacle_maps[blizzard_type]
-            inner_area = base_map[1:-1, 1:-1]
-
-            match blizzard_type:
-                case Tile.BlizzardUp:
-                    new_inner_area = np.concatenate(
-                        [inner_area[dy:, :], inner_area[:dy, :]], axis=0
-                    )
-                case Tile.BlizzardRight:
-                    new_inner_area = np.concatenate(
-                        [inner_area[:, (-1 * dx) :], inner_area[:, : (-1 * dx)]], axis=1
-                    )
-                case Tile.BlizzardDown:
-                    new_inner_area = np.concatenate(
-                        [inner_area[(-1 * dy) :, :], inner_area[: (-1 * dy), :]], axis=0
-                    )
-                case Tile.BlizzardLeft:
-                    new_inner_area = np.concatenate(
-                        [inner_area[:, dx:], inner_area[:, :dx]], axis=1
-                    )
-                case _:
-                    raise RuntimeError("Unexpected case. Possibly a bug")
-
-            new_map = np.zeros(shape=base_map.shape, dtype=bool)
-            new_map[1:-1, 1:-1] = new_inner_area
-
-            new_maps[blizzard_type] = new_map
+            new_maps[blizzard_type] = self.forecast_blizzard_for_turn_n(
+                n=n, blizzard_type=blizzard_type
+            )
 
         return new_maps
 
+    def four_directions(self, coord: tuple[int, int]) -> list[tuple[int, int]]:
+        output = []
+        y0, x0 = coord
+        for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            y1, x1 = y0 + dy, x0 + dx
+            if 0 <= y1 < self.height and 0 <= x1 < self.width:
+                output.append((y1, x1))
+
+        return output
+
+    def next_turn_moves(self, current_locations: np.ndarray) -> np.ndarray:
+        """Simulate all possible moves that Expedition can take in next turn.
+        Ignore any obstacles. Only consider boundries and the diffusion to four directions.
+
+        Args:
+            current_locations (np.ndarray): An np.array in bool that denote all possible current location of Expedition in this turn.
+
+        Returns:
+            np.ndarray: An np.array in bool that denote all possible moves for the Expedition to take in next turn.
+        """
+        new_state = np.zeros(shape=current_locations.shape, dtype=bool)
+
+        for coord in np.argwhere(current_locations == True):
+            four_neighbour_tiles = self.four_directions(coord)
+            for y, x in [coord, *four_neighbour_tiles]:
+                new_state[y, x] = True
+
+        return new_state
+
+    def simulate_next_turn(
+        self, current_locations: np.ndarray, next_turn_obstacles: ObstacleMaps
+    ) -> np.ndarray:
+        """Simulate all possible moves that Expedition can take in next turn.
+        This one take account of obstacles.
+        Tiles that the Expedition can exist in are denoted as True
+        """
+        all_moves = self.next_turn_moves(current_locations)
+        obstacles = sum(next_turn_obstacles.values()).astype(bool)  # pyright: ignore
+        all_possible_moves = all_moves & ~obstacles
+
+        return all_possible_moves
+
+    def bfs(
+        self, start: tuple[int, int], goal: tuple[int, int], starting_turn: int = 0
+    ) -> int:
+        initial_state = np.zeros(shape=(self.height, self.width), dtype=bool)
+        initial_state[start] = True
+
+        locations = initial_state
+        turn_number = starting_turn
+
+        while not locations[goal]:
+            next_turn_obstacles = self.make_obstacle_maps_for_turn_n(n=turn_number + 1)
+            locations = self.simulate_next_turn(locations, next_turn_obstacles)
+            turn_number += 1
+
+        return turn_number
+
 
 def parse_raw(raw: str) -> BlizzardBasin:
-    return BlizzardBasin(input_map=raw)
+    return BlizzardBasin(raw_string=raw)
 
 
-def part_one(data):
-    ...
+def part_one(basin: BlizzardBasin) -> int:
+    start = basin.start
+    goal = basin.goal
+    starting_turn = 0
+    return basin.bfs(start=start, goal=goal, starting_turn=starting_turn)
 
 
-def part_two(data):
-    ...
+def part_two(basin: BlizzardBasin) -> int:
+    start = basin.start
+    goal = basin.goal
+
+    first_trip = basin.bfs(start=start, goal=goal, starting_turn=0)
+    return_trip = basin.bfs(start=goal, goal=start, starting_turn=first_trip)
+    third_trip = basin.bfs(start=start, goal=goal, starting_turn=return_trip)
+
+    return third_trip
 
 
 if __name__ == "__main__":
--- tests/day_24_test.py
+++ tests/day_24_test.py
@@ -1,6 +1,7 @@
-import pytest
 import numpy as np
-from solutions.day_24 import parse_raw, part_one, part_two, BlizzardBasin, Tile
+import pytest
+
+from solutions.day_24 import BlizzardBasin, Tile, parse_raw, part_one, part_two
 
 simple_example = """#.######
 #>..<^.#
@@ -55,7 +56,7 @@ def test_convert_map_back_to_str():
 def test_obstacles_for_turn_1():
     basin = parse_raw(simple_example)
 
-    actual = basin.obstacles_for_turn_n(n=1)
+    actual = basin.make_obstacle_maps_for_turn_n(n=1)
 
     # wall doesn't change
     wall_map = actual[Tile.Wall]
@@ -88,54 +89,190 @@ def test_obstacles_for_turn_3():
 #......#
 ######.#"""
 
-    actual_obstacle_maps = basin.obstacles_for_turn_n(n=3)
+    actual_obstacle_maps = basin.make_obstacle_maps_for_turn_n(n=3)
     actual = basin.obstacle_map_as_string(actual_obstacle_maps)
 
     assert actual == expected
 
 
-@pytest.fixture(name="example_basin")
-def make_example_basin():
+@pytest.fixture(name="basin")
+def make_basin():
     yield parse_raw(example)
 
 
-def test_obstacles_for_turn_n__complex_example(example_basin):
-
-    each_turn = [
-        """#.######
+ExampleBasinFirst5Turns = [
+    """#.######
 #>>.<^<#
 #.<..<<#
 #>v.><>#
 #<^v^^>#
 ######.#""",
-        """#.######
+    """#.######
 #.>3.<.#
 #<..<<.#
 #>2.22.#
 #>v..^<#
 ######.#""",
-        """#.######
+    """#.######
 #.2>2..#
 #.^22^<#
 #.>2.^>#
 #.>..<.#
 ######.#""",
-        """#.######
+    """#.######
 #<^<22.#
 #.2<.2.#
 #><2>..#
 #..><..#
 ######.#""",
-        """#.######
+    """#.######
 #.<..22#
 #<<.<..#
 #<2.>>.#
 #.^22^.#
 ######.#""",
-    ]
+]
 
-    for turn_number, expected in enumerate(each_turn):
-        actual_obstacle_maps = example_basin.obstacles_for_turn_n(n=turn_number)
-        actual = example_basin.obstacle_map_as_string(actual_obstacle_maps)
+
+def test_make_obstacle_maps_for_turn_n__complex_example(basin):
+
+    for turn_number, expected in enumerate(ExampleBasinFirst5Turns):
+        actual_obstacle_maps = basin.make_obstacle_maps_for_turn_n(n=turn_number)
+        actual = basin.obstacle_map_as_string(actual_obstacle_maps)
 
         assert actual == expected
+
+
+def test_obstacle_forecast_cache_working(basin):
+    vertical_repeat_num = basin.inner_height
+    horizontal_repeat_num = basin.inner_width
+
+    def gcd(a, b):
+        if b == 0:
+            return a
+        return gcd(b, a % b)
+
+    lcm = (
+        vertical_repeat_num
+        * horizontal_repeat_num
+        // gcd(vertical_repeat_num, horizontal_repeat_num)
+    )
+
+    for turn_number in range(lcm):
+        expected = basin.make_obstacle_maps_for_turn_n(n=turn_number)
+        actual = basin.make_obstacle_maps_for_turn_n(n=(turn_number + lcm))
+
+        for obstacle_type in expected:
+            assert (actual[obstacle_type] == expected[obstacle_type]).all()
+
+
+def test_start_and_goal(basin):
+    expected = (0, 1)  # y, x
+    actual = basin.start
+
+    assert actual == expected
+
+    expected = (5, 6)  # y, x
+    actual = basin.goal
+
+    assert actual == expected
+
+
+def test_next_turn_moves(basin):
+    raw_input = """.E......
+........
+........
+........
+........
+........"""
+
+    raw_expected = """EEE.....
+.E......
+........
+........
+........
+........"""
+
+    input_state = basin.string_to_combined_map(raw_input).astype(bool)
+    expected = basin.string_to_combined_map(raw_expected).astype(bool)
+
+    actual = basin.next_turn_moves(current_locations=input_state)
+
+    assert (actual == expected).all()
+
+
+def test_next_turn_moves__multiple_locations(basin):
+    raw_input = """.E.E....
+........
+...E..E.
+....E...
+.E....E.
+........"""
+
+    raw_expected = """EEEEE...
+.E.E..E.
+..EEEEEE
+.E.EEEE.
+EEE.EEEE
+.E....E."""
+
+    input_state = basin.string_to_combined_map(raw_input).astype(bool)
+    expected = basin.string_to_combined_map(raw_expected).astype(bool)
+
+    actual = basin.next_turn_moves(current_locations=input_state)
+
+    assert (actual == expected).all()
+
+
+def test_simulate_next_turn(basin):
+    raw_input = """.E.E....
+........
+...E..E.
+....E...
+.E....E.
+........"""
+
+    next_turn_obstacles = basin.make_obstacle_maps_for_turn_n(n=1)
+    # """ #.######
+    #     #.>3.<.#
+    #     #<..<<.#
+    #     #>2.22.#
+    #     #>v..^<#
+    #     ######.#"""
+
+    raw_expected = """.E......
+.E....E.
+..EE..E.
+...E..E.
+....E...
+......E."""
+
+    input_state = basin.string_to_combined_map(raw_input).astype(bool)
+    expected = basin.string_to_combined_map(raw_expected).astype(bool)
+
+    actual = basin.simulate_next_turn(
+        current_locations=input_state, next_turn_obstacles=next_turn_obstacles
+    )
+
+    assert (actual == expected).all()
+
+
+def test_bfs(basin):
+    expected = 18
+    actual = basin.bfs(start=basin.start, goal=basin.goal, starting_turn=0)
+
+    assert actual == expected
+
+
+def test_part_one(basin):
+    expected = 18
+    actual = part_one(basin)
+
+    assert actual == expected
+
+
+def test_part_two(basin):
+    expected = 54
+    actual = part_two(basin)
+
+    assert actual == expected
